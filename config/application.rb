f89ea68e4d2fd74eefabb50fab558e18d88db273
Keith Gautreaux <keith.gautreaux@gmail.com>
2012-10-12 22:14:41 -0500

    * Added some initializers. * Removed unnecessary loading of some railties to speed up loading rails.

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,7 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+# require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -14,8 +12,6 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
-
-require 'devise/orm/active_record'
 
 module Medpasses
   class Application < Rails::Application
