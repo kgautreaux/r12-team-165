cd90c68d73486d59dbd7f0635e3c43dbbcd67f35
Keith Gautreaux <keith.gautreaux@gmail.com>
2012-10-26 20:02:09 -0500

    Fix sign_up by removing validation of twitter_username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :twitter_username
-  validates_presence_of :email, :twitter_username
+  validates_presence_of :email
 
   after_create do
     self.practice_id = 1
